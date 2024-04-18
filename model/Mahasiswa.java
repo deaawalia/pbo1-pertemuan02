1056d88dffe94378f022460e40290838304212e4
deaawalia <deaawaliafitriah@gmail.com>
2024-04-18 19:40:14 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+ System.out.println();
+ System.out.println("== Data Mahasiswa =====================");
+ System.out.println("NPM : 2210010535");
+ System.out.println("Nama : Dea Awalia Fitriah");
+ System.out.println("===================================");
+}
+}
